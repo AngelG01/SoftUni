2b15886e660e3a1ceb79803673a2342a05a6ba7a
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2021-03-10 15:31:19 +0200

    Add files via upload

---
+++ JavaOOP/Working with Abstraction/RhombusOfStars.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        System.out.println(getTriangleOfStars(n, true));
+        System.out.println(getTriangleOfStars(n, false));
+
+    }
+
+    public static String getTriangleOfStars(int n, boolean isUpwardsOrientated) {
+        StringBuilder out = new StringBuilder();
+
+        for (int r = 1; r <= n; r++) {
+            int spaceCount = isUpwardsOrientated ? n - r : r;
+            int starsCount = isUpwardsOrientated ? r : n - r;
+            out.append(repeatString(spaceCount, " "));
+            out.append(repeatString(starsCount, "* "));
+            if (r != n) {
+                out.append(System.lineSeparator());
+            }
+        }
+        return out.toString();
+    }
+
+    public static String repeatString(int count, String toRepeat) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            out.append(toRepeat);
+        }
+        return out.toString();
+    }
+
+}
